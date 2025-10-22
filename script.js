f95c04a415a62294d9ce6037668bfc109d3eb035
Nov3026 <alfredglaygbo92@gmail.com>
2025-10-22 09:20:00 +0000

    all

--- script.js
+++ script.js
@@ -16,18 +16,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// const hamburger = document.getElementById('hamburger');
-//   const closeIcon = document.getElementById('close-icon');
-//   const navDrawer = document.getElementById('nav-drawer');
 
-//   hamburger.addEventListener('click', () => {
-//     navDrawer.classList.add('active');
-//     hamburger.classList.add('active');
-//     closeIcon.classList.add('active');
-//   });
-
-//   closeIcon.addEventListener('click', () => {
-//     navDrawer.classList.remove('active');
-//     hamburger.classList.remove('active');
-//     closeIcon.classList.remove('active');
-//   });
